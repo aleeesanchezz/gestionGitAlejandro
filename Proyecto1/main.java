658fa51949b58e01237cb9d56680254d99a4f6f8
alejandro <alejandrosanchezmsyr@gmail.com>
2023-09-20 17:26:37 +0200

    Proyecto añadido

---
+++ Proyecto1/main.java
@@ -0,0 +1,10 @@
+package Proyecto1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hola, esta es la prueba del proyecto1");
+	}
+
+}
