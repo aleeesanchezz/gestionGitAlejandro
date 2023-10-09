3f9b106c6ec4381dc2fdd0a70f8c3f7172f84ec6
aleeesanchezz <145578438+aleeesanchezz@users.noreply.github.com>
2023-10-09 16:53:43 +0200

    Add files via upload

---
+++ DAW ASO/Programacion/T1/EJE1T1.java
@@ -0,0 +1,56 @@
+package Proyecto1;
+
+public class EJE1T1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ejercicio7();
+	
+	}
+public static void ejercicio4() {
+System.out.println("15:30- 16:25 \t BBDD \t FOL \t PRGMC \t BBDD \t ENTDL \n "
+		+ "16:25 - 17:20 \t BBDD \t ENTDL \t BBDD \t PRGMC \t BBDD \n"
+		+ "17:20 - 18:15 \t FOL \t PRGMC \t ENTDL \t BBDD \t FOL \n"
+		+ "18:15 - 18:45 \t R \t E \t CR \t E \t O \n"
+		+ "18:45 - 19:30 \t BBDD \t FOL \t PRGMC \t BBDD \t ENTDL \n "
+		+ "19:30 - 20:15 \t BBDD \t ENTDL \t BBDD \t PRGMC \t BBDD \n"
+		+ "20:15 - 21:00 \t FOL \t PRGMC \t ENTDL \t BBDD \t FOL \n");
+
+
+}
+public static void ejercicio5 (){
+    System.out.println("        *       ");
+    System.out.println("       * *       ");
+    System.out.println("      * * *      ");
+    System.out.println("     * * * *     ");
+    System.out.println("    * * * * *    ");
+    System.out.println("   * * * * * *   ");
+    System.out.println("  * * * * * * *  ");
+    System.out.println(" * * * * * * * * ");
+    System.out.println("* * * * * * * * *");
+
+}
+public  static void  ejercicio6 (){
+    System.out.println("        *       ");
+    System.out.println("       * *       ");
+    System.out.println("      *   *      ");
+    System.out.println("     *     *     ");
+    System.out.println("    *       *    ");
+    System.out.println("   *         *   ");
+    System.out.println("  *           *  ");
+    System.out.println(" *             * ");
+    System.out.println("* * * * * * * * *");
+}
+public static void ejercicio7() {
+    System.out.println("* * * * * * * * *");
+    System.out.println(" * * * * * * * * ");
+    System.out.println("  * * * * * * *  ");
+    System.out.println("   * * * * * *   ");
+    System.out.println("    * * * * *    ");
+    System.out.println("     * * * *     ");
+    System.out.println("      * * *      ");
+    System.out.println("       * *       ");
+    System.out.println("        *       ");
+}
+}
+
